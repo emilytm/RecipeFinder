b4db688be6265a4bf622943fd025d14e4b3fd91e
emilytm <emilytmcintyre@gmail.com>
2022-12-20 13:00:15 -0500

    Starting js for rendering list of recipes

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 import recipes from "./recipes.js"
 
 console.log(recipes[0])
+
+renderRecipes(recipes)
+
+function renderRecipes(recipes) {
+    let recipeListHtml = ""
+    for (let recipe of recipes) {
+        recipeListHtml += getRecipeHtml(recipe)
+    }
+}
+
+function getRecipeHtml(recipe) {
+    let newHtml = ""
+    newHtml = `
+        <div class="recipe-box">
+            <img class="recipe-image" src="${recipe.image}" alt="Picture of selected recipe">
+            <h3 class="recipe-title">${recipe.title}</h3>
+            <p class="recipe-source">${recipe.source}</p>
+            <p class="recipe-tags">${recipe.tags}</p>
+            <p class="recipe-time">${recipe.time.active}</p>
+            <p class="recipe-ingredients">${recipe.ingredients}</p>
+            <p class="recipe-made">${recipe.lastmade}</p>
+         </div>
+    `
+    console.log(newHtml)
+}
