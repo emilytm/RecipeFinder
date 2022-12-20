b367f85f5b860a6dd18dc6e47fefe3a1a0fd7a33
emilytm <emilytmcintyre@gmail.com>
2022-12-20 12:34:18 -0500

    Connecting js and adding initial recipe data

---
+++ recipes.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        name: 'Classic Croissants',
+        source: 'NYT Cooking',
+        tags: ['French', 'Classic', 'Baking'],
+        lastmade: 'November 18, 2020',
+        ingredients: ['Butter', 'Flour', 'Sugar', 'Yeast'],
+        time: {
+            active: '4 hrs', 
+            total: '14 hrs'
+        }
+    },
+    {
+        id: 1,
+        name: 'Tahini Billionaire Bars',
+        source: 'Bon Appetit',
+        tags: ['Baking','Twist'],
+        lastmade: 'June 18, 2021',
+        ingredients: ['Butter', 'Flour', 'Sugar', 'Yeast'],
+        time: {
+            active: '1 hrs',
+            total: '2 hrs'
+        }
+    },
+    {
+        id: 1,
+        name: 'Triple Coconut Cake',
+        source: 'Dessert Person',
+        tags: ['Cake', 'Birthday', 'Baking','Coconut'],
+        lastmade: 'December 10, 2022',
+        ingredients: ['Coconut milk', 'Flour', 'Sugar', 'Buttermilk'],
+        time: {
+            active: '2.5 hrs',
+            total: '3.25 hrs'
+        }
+    }
+]
