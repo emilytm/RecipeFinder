adeab580342c05a3a415aefc54c3991a9908f27c
emilytm <emilytmcintyre@gmail.com>
2022-12-21 17:11:56 -0500

    Updated search UI to be more intuitive on mobile

--- index.js
+++ index.js
@@ -70,11 +70,11 @@ function getRecipeHtml(recipe) {
         <div class="recipe-box" data-recipe=${recipe.id}>
             <img class="recipe-image" data-recipe=${recipe.id} src="${recipe.image}" alt="Picture of ${recipe.title}">
             <h3 class="recipe-title" data-recipe=${recipe.id}>${recipe.title}</h3>
-            <p class="recipe-source" data-recipe=${recipe.id}>${recipe.source}</p>
-            <p class="recipe-tags" data-recipe=${recipe.id}>${recipe.tags}</p>
-            <p class="recipe-time" data-recipe=${recipe.id}>${recipe.time}</p>
-            <p class="recipe-ingredients" data-recipe=${recipe.id}>${recipe.ingredients}</p>
-            <p class="recipe-made" data-recipe=${recipe.id}>${recipe.lastmade}</p>
+            <p class="recipe-source grid-right" data-recipe=${recipe.id}>From: ${recipe.source}</p>
+            <p class="recipe-tags" data-recipe=${recipe.id}>Tags: ${recipe.tags}</p>
+            <p class="recipe-time grid-right" data-recipe=${recipe.id}>${recipe.time}</p>
+            <p class="recipe-ingredients" data-recipe=${recipe.id}>Ingredient highlight: ${recipe.ingredients}</p>
+            <p class="recipe-made grid-right" data-recipe=${recipe.id}>Last made: ${recipe.lastmade}</p>
          </div>
     `
     return newHtml
