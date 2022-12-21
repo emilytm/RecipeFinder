b82f2ddb86cff88dac6b4f56ec298e4f0da67111
emilytm <emilytmcintyre@gmail.com>
2022-12-21 12:36:21 -0500

    Getting search started

--- index.js
+++ index.js
@@ -4,13 +4,42 @@ console.log(recipes[0])
 
 renderRecipes(recipes)
 
+const searchBar = document.getElementById('search')
+searchBar.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    let searchTerm = searchBar.elements.search.value
+    searchRecipes(searchTerm.trim())
+    searchBar.elements.search.value = ""
+})
+
 document.addEventListener('click',function(e){
     if(e.target.dataset.recipe){
         console.log("RECIPE CLICKED")
     } else {
         console.log("SOMETHING CLICKED")
     }
+
+    console.log(e)
 })
+
+function searchRecipes(searchTerm) {
+    console.log(searchTerm)
+    let searchResults = []
+
+    searchResults = recipes.filter(recipe => {
+        if (recipe.title.includes(searchTerm)){
+            console.log("Found ",searchTerm," in ",recipe.title)
+            return true
+        } else {
+            console.log("Did not find ",searchTerm," in ",recipe.title)
+            return false
+        }
+    })
+    console.log(searchResults)
+    //search recipe by recipe to see if it includes the term
+    //display the array of new recipes in the list
+}
 
 function renderRecipes(recipes) {
     const recipeList = document.getElementById('recipe-list')
