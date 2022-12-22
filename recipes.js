f08b67dbfaef6ae79970c2c9e2d27edf44454f74
emilytm <emilytmcintyre@gmail.com>
2022-12-22 17:44:27 -0500

    Updated some styling and assets

--- recipes.js
+++ recipes.js
@@ -31,5 +31,49 @@ export default [
         lastmade: 'December 10, 2022',
         ingredients: 'Malted Milk Powder, Dutch Process Cocoa Powder',
         time: '45 min active, 1.5 hrs total'
+    },
+    {
+        id: 4,
+        title: 'Stuffed Mushrooms',
+        source: 'NYT Cooking',
+        link: 'https://cooking.nytimes.com/recipes/1022390-stuffed-mushrooms?action=click&module=RecipeBox&pgType=recipebox-page&region=all&rank=144',
+        image: 'https://static01.nyt.com/images/2021/11/04/dining/kc-stuffed-mushrooms/kc-stuffed-mushrooms-articleLarge.jpg?w=1280&q=75',
+        tags: 'Cooking, Appetizer, Vegetarian, Quick, Easy',
+        lastmade: 'July 9, 2022',
+        ingredients: 'Panko, Cremini Mushrooms',
+        time: '25 min'
+    },
+    {
+        id: 5,
+        title: 'Green Beans and Greens With Fried Shallots',
+        source: 'NYT Cooking',
+        link: 'https://cooking.nytimes.com/recipes/1020603-green-beans-and-greens-with-fried-shallots?action=click&module=RecipeBox&pgType=recipebox-page&region=all&rank=143',
+        image: 'https://static01.nyt.com/images/2019/11/13/dining/11Romanrex4/merlin_161935386_137071aa-6441-4ccc-a962-f0b0e4b0f90c-articleLarge.jpg?w=1280&q=75',
+        tags: 'Cooking, Vegetarian, Side, Easy, Quick',
+        lastmade: 'Not yet',
+        ingredients: 'Green Beans, Kale',
+        time: '20 min'
+    },
+    {
+        id: 6,
+        title: 'Creamy Cauliflower Soup With Rosemary Olive Oil',
+        source: 'NYT Cooking',
+        link: 'https://cooking.nytimes.com/recipes/1020764-creamy-cauliflower-soup-with-rosemary-olive-oil?action=click&module=RecipeBox&pgType=recipebox-page&region=dinner&rank=45',
+        image: 'https://static01.nyt.com/images/2020/01/02/dining/lh-cauliflower-soup/merlin_166521297_2fda2604-b728-4d3f-8eaa-f9ca310e78f9-articleLarge.jpg?w=1280&q=75',
+        tags: 'Soup, Healthy, Easy',
+        lastmade: 'December 24, 2021',
+        ingredients: 'Rosemary, Cauliflower',
+        time: '40 min'
+    },
+    {
+        id: 7,
+        title: 'Mushrooms and Greens with Toast',
+        source: 'Smitten Kitchen',
+        link: 'https://smittenkitchen.com/2015/05/mushrooms-and-greens-with-toast/',
+        image: 'https://i0.wp.com/smittenkitchen.com/wp-content/uploads//2015/05/mushroooms-and-greens-with-toast1.jpg?fit=640%2C427&amp;ssl=1',
+        tags: 'Breakfast, Brunch, Vegetarian, Quick, Easy',
+        lastmade: 'Not yet',
+        ingredients: 'Mixed Mushrooms, Country Bread, Fresh Red Chile, Hearty Greens, Fontina',
+        time: '25 min'
     }
 ]
