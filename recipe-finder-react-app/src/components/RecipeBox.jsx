8bd368a64440ae4f735fac648d2c7bee3bdd342f
emilytm <113457923+emilytm@users.noreply.github.com>
2023-03-24 12:02:21 -0400

    Converted recipe page files

---
+++ recipe-finder-react-app/src/components/RecipeBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RecipeBox(){
+    return (
+        <div id="recipe-display" class="recipe-display">
+            <div id="recipe-details">
+            </div>
+        </div>
+    )
+};
